e6fdf5720ccbd25094ab3128f45468f36ea4b18c
Наталия <natalia@MacBook-Air-Natalia.local>
2015-03-23 16:50:43 +0300

    Working tableView

---
+++ Homework2/BookmarkTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookmarkTableViewController.h
+//  Homework2
+//
+//  Created by Наталия on 23.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookmarkTableViewController : UITableViewController
+
+@end
---
+++ Homework2/BookmarkTableViewController.m
@@ -0,0 +1,118 @@
+//
+//  BookmarkTableViewController.m
+//  Homework2
+//
+//  Created by Наталия on 23.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import "BookmarkTableViewController.h"
+
+@interface BookmarkTableViewController ()
+
+@property (strong, nonatomic) NSArray *bookmarks;
+
+@end
+
+@implementation BookmarkTableViewController
+
+-(id) initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if(self)
+    {
+        self.bookmarks = @[@"http://apple.com",
+                           @"http://google.com",
+                           @"http://yahoo.com",
+                           @"http://vk.com",
+                           @"http://facebook.com",
+                           @"http://ya.ru",
+                           @"http://mail.ru",];
+    }
+    return self;
+}
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 7;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:reuseIdentifier];
+    
+    [cell setText:[NSString stringWithFormat:@"%@",self.bookmarks[indexPath.row]]];
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
