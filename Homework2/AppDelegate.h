ad28113c6b682d17a3643cb8bf73f3f8fb941971
Наталия <natalia@MacBook-Air-Natalia.local>
2015-03-23 16:32:35 +0300

    Initial commit

---
+++ Homework2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework2
+//
+//  Created by Наталия on 23.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework2
+//
+//  Created by Наталия on 23.03.15.
+//  Copyright (c) 2015 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
